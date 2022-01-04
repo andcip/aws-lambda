2f31018fbf2e723b88bbd3ada582f17d019b0c09
Andrea Cipriani <andrea.cipriani@assist.it>
2022-01-04 15:30:59 +0100

    Add openapi file to rest module

--- triggers/rest/main.tf
+++ triggers/rest/main.tf
@@ -1,13 +1,25 @@
-## API GATEWAY
+locals {
+
+  api_routes = [for route in var.trigger.routes: {
+    path: route.path
+    parameter: regexall( "{", route.path ) ? substr( route.path , index(split("", route.path), "{") +1, index(split("", route.path), "}") - index(split("", route.path), "{") -1) : ""
+    method: route.method
+  }]
+
+}
 
 resource "aws_api_gateway_rest_api" "api" {
-  name = "${var.lambda_function_name}-api" ##TODO VARIABLE
-  #api_key_source = "HEADER"
+  name = "${var.lambda_function_name}-api"
+
+  body = templatefile("${path.module}/openapi.tftpl", {routes: local.api_routes, lambda_invoke_arn: var.lambda_function_invoke_arn})
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
 
-  depends_on  = [aws_api_gateway_method.api_method, aws_api_gateway_resource.api_resource]
   rest_api_id = aws_api_gateway_rest_api.api.id
   triggers    = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api.body))
@@ -43,31 +55,6 @@ resource "aws_api_gateway_stage" "stage" {
 
 }
 
-resource "aws_api_gateway_integration" "integration" {
-  count                   = length(var.trigger.routes)
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.api_resource[count.index].id
-  http_method             = aws_api_gateway_method.api_method[count.index].http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = var.lambda_function_invoke_arn
-}
-
-resource "aws_api_gateway_resource" "api_resource" {
-  count       = length(var.trigger.routes)
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = var.trigger.routes[count.index].path
-
-}
-
-resource "aws_api_gateway_method" "api_method" {
-  count         = length(var.trigger.routes)
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.api_resource[count.index].id
-  http_method   = var.trigger.routes[count.index].method
-  authorization = "NONE"
-}
 
 
 resource "aws_cloudwatch_log_group" "apigateway_log_group" {
