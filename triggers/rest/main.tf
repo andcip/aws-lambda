f625b2d8f98acec201829634f95ae60d43c16a77
Andrea Cipriani <andrea.cipriani@assist.it>
2022-05-10 14:44:01 +0200

    Fix provider error on redeployment trigger

--- triggers/rest/main.tf
+++ triggers/rest/main.tf
@@ -42,7 +42,7 @@ resource "aws_api_gateway_rest_api" "api" {
 
 locals {
   body = jsonencode(aws_api_gateway_rest_api.api.body)
-  redeployment_sha = var.trigger.resource_policy != null ? sha1( "${local.body}_${jsonencode(aws_api_gateway_rest_api_policy.resource_policy[0].policy)}") : sha1(local.body)
+  redeployment_sha = var.trigger.resource_policy != null ? sha1( "${local.body},${jsonencode(jsondecode(aws_api_gateway_rest_api_policy.resource_policy[0].policy))}") : sha1(local.body)
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
